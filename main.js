42201bf2afeb0e1aba32d184e8450859cbd42bf3
Billyrosecodes <tolulopeadeyemi2020@gmail.com>
2023-06-08 18:20:26 -0700

    debugging in process

--- main.js
+++ main.js
@@ -15,12 +15,29 @@ document.addEventListener('DOMContentLoaded'),function(){
     const addForm = forms['add-movie'];
     addForm.addEventListener('submit',function(e){
     e.preventDefault();
-    })
-
-
+    
 
 
     //creating elements
+    const value = addForm.querySelector('input[type="text"]').value;
+    const li = document.createElement('li');
+    const movieName = document.createElement('span');
+    const deleteBtn = document.createElement('span');
 
+
+    //add text content
+     movieName.textContent = value;
+     deleteBtn.textContent = 'delete';
+
+     //add classes
+     movieName.classList.add('name');
+     deleteBtn.classList.add('delete');
+
+     //append to DOM
+     li.appendChild(movieName);
+     li.appendChild(deleteBtn);
+     li.appendChild(li);
+
+    });
     
 }
